e7baa746f854bf4aad7c129837c09191f69f8512
Özgür Barış Ayhan <barisayhan01@gmail.com>
2022-07-01 20:37:28 +0300

    statikZaman

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 let currentTime = new Date();
 
 const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var zaman = currentTime.getHours() + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds() + " " + weekday[currentTime.getDay()];
 
-console.log(currentTime.getHours() + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds() + " " + weekday[currentTime.getDay()]);
-
-
-
-
+document.getElementById("time").innerText = zaman;
 
 
 
