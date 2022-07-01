151d9e618c6fbc863ddd653e6583b1000c02d3b3
Özgür Barış Ayhan <barisayhan01@gmail.com>
2022-07-01 21:02:55 +0300

    update

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-let currentTime = new Date();
-
-const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var zaman = currentTime.getHours() + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds() + " " + weekday[currentTime.getDay()];
-
-document.getElementById("time").innerText = zaman;
-
-
-
 var isim = prompt("lütfen isim giriniz: ");
 
 if (isim != null) {
     document.getElementById("myName").innerText = `Merhaba ${isim} Hoşgeldin;`;    
 }
 
+
+
+let currentTime = new Date();
+
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// var zaman = currentTime.getHours() + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds() + " " + weekday[currentTime.getDay()];
+
+document.getElementById("time").innerText = currentTime.getHours() + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds() + " " + weekday[currentTime.getDay()];
