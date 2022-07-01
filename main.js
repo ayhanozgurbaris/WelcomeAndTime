22e4c402d1759b4aa1f38810b14afdc23f3efd82
Özgür Barış Ayhan <barisayhan01@gmail.com>
2022-07-01 21:19:09 +0300

    update

--- main.js
+++ main.js
@@ -5,16 +5,19 @@ if (isim != null) {
 }
 
 
+function timeUpdate() {
 
+    let currentTime = new Date();
 
-let currentTime = new Date();
+    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    var gun = currentTime.getDay();
 
-var hours = currentTime.getHours();
-var minutes = currentTime.getMinutes();
-var seconds = currentTime.getSeconds();
-var gun = currentTime.getDay();
+    document.getElementById("time").innerText = hours + ":" + minutes + ":" + seconds + " " + weekday[gun];
+}
 
-document.getElementById("time").innerText = hours + ":" + minutes + ":" + seconds + " " + weekday[gun];
+setInterval(timeUpdate,1000);
 
